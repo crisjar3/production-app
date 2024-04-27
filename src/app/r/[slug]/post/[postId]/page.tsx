7d95c2165acc2d486ce73181a8b0226021188593
Cristhian Gomez <crgomez@assanet.com>
2024-04-27 10:32:51 -0600

    fix visaulize single product

--- src/app/r/[slug]/post/[postId]/page.tsx
+++ src/app/r/[slug]/post/[postId]/page.tsx
@@ -21,13 +21,11 @@ export const dynamic = 'force-dynamic'
 export const fetchCache = 'force-no-store'
 
 const SubRedditPostPage = async ({ params }: SubRedditPostPageProps) => {
-  const cachedPost = (await redis.hgetall(
-    `post:${params.postId}`
-  )) as CachedPost
+  const cachedPost = {} as CachedPost
 
   let post: (Post & { votes: Vote[]; author: User }) | null = null
 
-  if (!cachedPost) {
+  if (!cachedPost.content) {
     post = await db.post.findFirst({
       where: {
         id: params.postId,
@@ -46,7 +44,7 @@ const SubRedditPostPage = async ({ params }: SubRedditPostPageProps) => {
       <div className='h-full flex flex-col sm:flex-row items-center sm:items-start justify-between'>
         <Suspense fallback={<PostVoteShell />}>
           {/* @ts-expect-error server component */}
-          <PostVoteServer
+          {/* <PostVoteServer
             postId={post?.id ?? cachedPost.id}
             getData={async () => {
               return await db.post.findUnique({
@@ -58,19 +56,19 @@ const SubRedditPostPage = async ({ params }: SubRedditPostPageProps) => {
                 },
               })
             }}
-          />
+          /> */}
         </Suspense>
 
         <div className='sm:w-0 w-full flex-1 bg-white p-4 rounded-sm'>
           <p className='max-h-40 mt-1 truncate text-xs text-gray-500'>
             Posted by u/{post?.author.username ?? cachedPost.authorUsername}{' '}
-            {formatTimeToNow(new Date(post?.createdAt ?? cachedPost.createdAt))}
+            {formatTimeToNow(new Date(post?.createdAt ?? new Date()))}
           </p>
           <h1 className='text-xl font-semibold py-2 leading-6 text-gray-900'>
             {post?.title ?? cachedPost.title}
           </h1>
 
-          <EditorOutput content={post?.content ?? cachedPost.content} />
+          <EditorOutput content={post?.content ?? "No COntente"} />
           <Suspense
             fallback={
               <Loader2 className='h-5 w-5 animate-spin text-zinc-500' />
